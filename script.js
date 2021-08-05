7c3d3ebe9e98d3bd42e7cfbfaf360652d38464c5
Sarah Abu Zeneh <sara.abu.zena2000@gmail.com>
2021-08-05 16:49:58 +0300

    Edit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const urlCompleated ="https://jsonplaceholder.typicode.com/todos/todoID";
 let toDoArray = [];
 
 function fetchData(){
-   fetch(url).then(response =>{
+   fetch("https://610990bad71b6700176399bd.mockapi.io/todos").then(response =>{
        return response.json();
    }).then(data => {
        data.forEach(item => {
@@ -57,86 +57,6 @@ function show(){
         objectToDo.appendChild(buttonTrash);
         toDoContainer.appendChild(objectToDo);
         buttonTrash.addEventListener('click',function(){
-            
-            let deleteBtnIsPressed=e.target.id="delete-btn"
-            let id=e.target.parentElement.dataset.id;
-            if(deleteBtnIsPressed){
-            fetch(`${url}/${id}`,{
-            method:'DELETE',
-            })
-            .then(response=>response.json())
-            .then(()=>location.reload())
-            }
-            
-            })
-            lists.addEventListener('click', (e)=> {
-                let deleteBtnIsPressed=e.target.id="delete-btn"
-            let id=e.target.parentElement.dataset.id;
-            if(deleteBtnIsPressed){
-            fetch(`${api_url}/${id}`,{
-            method:'DELETE',
-            })
-            .then(response=>response.json())
-            .then(()=>location.reload())
-            }
-            
-            })
-        checkLabel.addEventListener('change', function(){
-            if(checkLabel.checked == true){
-                console.log("true");
-                checkLabel.disabled=true;
-                timeCr.innerHTML="Done";
-                objectToDo.classList.remove("backColor");
-                objectToDo.classList.add("done");
-                
-            }else{
-                objectToDo.classList.add("backColor");
-                objectToDo.classList.remove("done");
-                itemContainer.removeChild(minute);
-        
-            }
-           
-            
-        })
-
-    })
-    
-
-}
-show();
-addBtn.addEventListener('click', function() {
-   console.log("sth");
-
-   let toDoContainer = document.querySelector(".toDo"); 
-    let objectToDo = document.createElement("div"); 
-    objectToDo.className = "toDoItem , backColor";
-    let itemContainer =  document.createElement("div");
-    itemContainer.className = "check";
-    let checkLabel = document.createElement("input");
-    checkLabel.type="checkbox";
-    checkLabel.className="checB";
-    let item = document.createElement("span");
-    item.className="itemSpan";
-    let timeCr=document.createElement("small");
-    let buttonTrash= document.createElement("button");
-    buttonTrash.className="trash";
-    buttonTrash.innerHTML='<i class="fas fa-trash-alt trashIcon"></i>';
-    item.innerHTML=inputContent.value ;
-    itemContainer.appendChild(checkLabel);
-    itemContainer.appendChild(item);
-    itemContainer.appendChild(timeCr);
-    objectToDo.appendChild(itemContainer);
-    objectToDo.appendChild(buttonTrash);
-    toDoContainer.appendChild(objectToDo);
-    item.innerHTML="";
-    
-    clearItem.addEventListener('click',function(){
-        toDoContainer.removeChild(objectToDo);
-        spanCounter.innerHTML= 0;
-    
-        })
-        spanCounter.innerHTML=toDoArray.length;
-        buttonTrash.addEventListener('click',function(){
             toDoContainer.removeChild(objectToDo);
             counter--;
             spanCounter.innerHTML=counter;
@@ -162,7 +82,81 @@ addBtn.addEventListener('click', function() {
                 }
                 
             })
+
+    })
+    
+
+}
+show();
+addBtn.addEventListener('click', function() {
+   
+
+    item.innerHTML=inputContent.value ;
+    itemContainer.appendChild(checkLabel);
+    itemContainer.appendChild(item);
+    itemContainer.appendChild(timeCr);
+    objectToDo.appendChild(itemContainer);
+    objectToDo.appendChild(buttonTrash);
+    toDoContainer.appendChild(objectToDo);
+    item.innerHTML="";
+    buttonTrash.addEventListener('click',function(){
+        toDoContainer.removeChild(objectToDo);
+        counter--;
+        spanCounter.innerHTML=counter;
+        if(counter == 0){
+            clearItem.classList.remove("buttonApperance");
         
+        }
+    
+        })
+        checkLabel.addEventListener('change', function(){
+            if(checkLabel.checked == true){
+                console.log("true");
+                checkLabel.disabled=true;
+                timeCr.innerHTML="Done";
+                objectToDo.classList.remove("backColor");
+                objectToDo.classList.add("done");
+                
+            }else{
+                objectToDo.classList.add("backColor");
+                objectToDo.classList.remove("done");
+                itemContainer.removeChild(minute);
+        
+            }
+            
+        })
+    
     })
 
-      
+        clearItem.addEventListener('click',function(){
+            toDoContainer.removeChild(objectToDo);
+            spanCounter.innerHTML= 0;
+        
+            })
+            spanCounter.innerHTML=toDoArray.length;
+            buttonTrash.addEventListener('click',function(){
+                toDoContainer.removeChild(objectToDo);
+                counter--;
+                spanCounter.innerHTML=counter;
+                if(counter == 0){
+                    clearItem.classList.remove("buttonApperance");
+                
+                }
+            
+                })
+                checkLabel.addEventListener('change', function(){
+                    if(checkLabel.checked == true){
+                        console.log("true");
+                        checkLabel.disabled=true;
+                        timeCr.innerHTML="Done";
+                        objectToDo.classList.remove("backColor");
+                        objectToDo.classList.add("done");
+                        
+                    }else{
+                        objectToDo.classList.add("backColor");
+                        objectToDo.classList.remove("done");
+                        itemContainer.removeChild(minute);
+                
+                    }
+                    
+                })

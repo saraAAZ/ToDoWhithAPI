e107c1063f010c1df2d1ba18c150afd0c8659421
Sarah Abu Zeneh <sara.abu.zena2000@gmail.com>
2021-08-05 16:35:49 +0300

    ToDo With AIP

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ function fetchData(){
            addElement(item);
        });
     //   console.log(toDoArray);
+    console.log(toDoArray);
+    show();
    }
     ).catch(error=> {
         console.log(error);
@@ -26,20 +28,13 @@ function fetchData(){
 
 function addElement(item){
     toDoArray.push(item);
-    
-
 }
 fetchData();
-console.log(toDoArray);
-// const fetchto= async(url)=>{
-//     fetch(`${url}`).then(res=>res.json()).then(data=>data.forEach(item=>toDoArray.push(item)))
-// }
-// console.log(toDoArray);
-// if(inputContent.value.trim() ==0 && timeBtn.value.trim()==0){
-//     addBtn.classList.add("active");
-// }else (inputContent.value.trim() !=0 && timeBtn.value.trim()!=0){
-//     addBtn.classList.remove("active");
-// }
+
+function show(){
+    
+    console.log(toDoArray);
+    toDoArray.forEach(input=>{
     let toDoContainer = document.querySelector(".toDo"); 
     let objectToDo = document.createElement("div"); 
     objectToDo.className = "toDoItem , backColor";
@@ -53,55 +48,29 @@ console.log(toDoArray);
     let timeCr=document.createElement("small");
     let buttonTrash= document.createElement("button");
     buttonTrash.className="trash";
-    // buttonTrash.innerHTML='<i class="fas fa-trash-alt trashIcon"></i>';
-    
-
-function show(){
-    
-    toDoArray.forEach(input=>{
-        
-        item.innerHTML= inputContent.value;
+    buttonTrash.innerHTML='<i class="fas fa-trash-alt trashIcon"></i>';
+        item.innerHTML=input.title;
         itemContainer.appendChild(checkLabel);
         itemContainer.appendChild(item);
         itemContainer.appendChild(timeCr);
         objectToDo.appendChild(itemContainer);
         objectToDo.appendChild(buttonTrash);
         toDoContainer.appendChild(objectToDo);
-
-
-    })
-    
-
-}
-show();
-addBtn.addEventListener('click', function() {
-    
-    
-
-       
-
-
-
-    
-    })
-
-    
-    buttonTrash.addEventListener('click',function(){
-        toDoContainer.removeChild(objectToDo);
-        counter--;
-        spanCounter.innerHTML=counter;
-        if(counter == 0){
-            clearItem.classList.remove("buttonApperance");
+        buttonTrash.addEventListener('click',function(){
+            toDoContainer.removeChild(objectToDo);
+            counter--;
+            spanCounter.innerHTML=counter;
+            if(counter == 0){
+                clearItem.classList.remove("buttonApperance");
+            
+            }
         
-        }
-    
-        })
+            })
         checkLabel.addEventListener('change', function(){
             if(checkLabel.checked == true){
                 console.log("true");
                 checkLabel.disabled=true;
                 timeCr.innerHTML="Done";
-                itemContainer.removeChild(minute);
                 objectToDo.classList.remove("backColor");
                 objectToDo.classList.add("done");
                 
@@ -111,12 +80,74 @@ addBtn.addEventListener('click', function() {
                 itemContainer.removeChild(minute);
         
             }
+           
             
         })
-        
-        clearItem.addEventListener('click',function(){
+
+    })
+    
+
+}
+show();
+addBtn.addEventListener('click', function() {
+   console.log("sth");
+
+   let toDoContainer = document.querySelector(".toDo"); 
+    let objectToDo = document.createElement("div"); 
+    objectToDo.className = "toDoItem , backColor";
+    let itemContainer =  document.createElement("div");
+    itemContainer.className = "check";
+    let checkLabel = document.createElement("input");
+    checkLabel.type="checkbox";
+    checkLabel.className="checB";
+    let item = document.createElement("span");
+    item.className="itemSpan";
+    let timeCr=document.createElement("small");
+    let buttonTrash= document.createElement("button");
+    buttonTrash.className="trash";
+    buttonTrash.innerHTML='<i class="fas fa-trash-alt trashIcon"></i>';
+    item.innerHTML=inputContent.value ;
+    itemContainer.appendChild(checkLabel);
+    itemContainer.appendChild(item);
+    itemContainer.appendChild(timeCr);
+    objectToDo.appendChild(itemContainer);
+    objectToDo.appendChild(buttonTrash);
+    toDoContainer.appendChild(objectToDo);
+    item.innerHTML="";
+    
+    clearItem.addEventListener('click',function(){
+        toDoContainer.removeChild(objectToDo);
+        spanCounter.innerHTML= 0;
+    
+        })
+        spanCounter.innerHTML=toDoArray.length;
+        buttonTrash.addEventListener('click',function(){
             toDoContainer.removeChild(objectToDo);
-            spanCounter.innerHTML= 0;
+            counter--;
+            spanCounter.innerHTML=counter;
+            if(counter == 0){
+                clearItem.classList.remove("buttonApperance");
+            
+            }
         
             })
-            spanCounter.innerHTML=toDoArray.length;
+            checkLabel.addEventListener('change', function(){
+                if(checkLabel.checked == true){
+                    console.log("true");
+                    checkLabel.disabled=true;
+                    timeCr.innerHTML="Done";
+                    objectToDo.classList.remove("backColor");
+                    objectToDo.classList.add("done");
+                    
+                }else{
+                    objectToDo.classList.add("backColor");
+                    objectToDo.classList.remove("done");
+                    itemContainer.removeChild(minute);
+            
+                }
+                
+            })
+        
+    })
+
+      
